33a47c4eddb4f980bdee229b81a188ca968692b9
Santiago López Rey <devsantilopez@gmail.com>
2022-01-25 21:39:00 +0100

    Cambiado aplicación de velocidad a jugadores

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,16 +11,20 @@ public class Player : MonoBehaviour
     GameObject p1;
 
     GameObject p2;
+
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        speed = 5f;
+        speed = 10f;
 
         actualSpeed = speed;
 
         p1 = GameObject.Find("P1");
 
         p2 = GameObject.Find("P2");
+
+        //rb = GetComponent<Rigidbody2D>();
 
         if (p1 == null || p2 == null){
             Debug.Log("Algún jugador sin inicializar");
@@ -34,21 +38,25 @@ public class Player : MonoBehaviour
         if(Input.GetKey(KeyCode.W)){
 
             //p1.transform.Translate(Vector3.up*speed*Time.deltaTime);
-            p1.transform.Translate(Vector3.up * actualSpeed * Time.deltaTime, Space.Self);
+            //p1.transform.Translate(Vector3.up * actualSpeed * Time.deltaTime, Space.Self);
+            p1.GetComponent<Rigidbody2D>().velocity = new Vector3(0,speed,0);
 
         }else if(Input.GetKey(KeyCode.S)){
 
             //p1.transform.Translate(Vector3.down*speed*Time.deltaTime);
-            p1.transform.Translate(Vector3.down * actualSpeed * Time.deltaTime, Space.Self);
+            //p1.transform.Translate(Vector3.down * actualSpeed * Time.deltaTime, Space.Self);
+            p1.GetComponent<Rigidbody2D>().velocity = new Vector3(0,-speed,0);
         }
         
          if(Input.GetKey(KeyCode.UpArrow)){
 
-            p2.transform.Translate(Vector3.up*speed*Time.deltaTime);
+            //p2.transform.Translate(Vector3.up*speed*Time.deltaTime);
+            p2.GetComponent<Rigidbody2D>().velocity = new Vector3(0,speed,0);
 
         }else if(Input.GetKey(KeyCode.DownArrow)){
 
-            p2.transform.Translate(Vector3.down*speed*Time.deltaTime);
+            //p2.transform.Translate(Vector3.down*speed*Time.deltaTime);
+            p2.GetComponent<Rigidbody2D>().velocity = new Vector3(0,-speed,0);
 
         } 
     }
