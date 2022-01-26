b1777e36d26142a19bbc6a5c6cc1305cb0f2e9f2
Santiago López Rey <devsantilopez@gmail.com>
2022-01-26 21:46:47 +0100

    Añadida corrutina para spawn de bola

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,41 +6,43 @@ public class Ball : MonoBehaviour
 {
     float speed;
 
-    GameObject ball;
+    //private GameObject ball;
+
+    private Vector2 spawnPoint = new Vector2(0,0);
+
+    public Vector2 SpawnPoint{
+        get { return spawnPoint;}
+    }
     // Start is called before the first frame update
     void Start()
     {
         speed = -5f;
 
-        ball = GameObject.Find("Ball");
+        //ball = GameObject.Find("Ball");
     }
 
     // Update is called once per frame
     void Update()
     {
-        ball.GetComponent<Rigidbody2D>().velocity = new Vector3(speed, 0, 0);
+        this.GetComponent<Rigidbody2D>().velocity = new Vector3(speed, 0, 0);
     }
 
     public void OnCollisionEnter2D(Collision2D colision) {
+ 
+       if(colision.gameObject.tag=="Player"){
 
-       // Debug.Log("Baia, que golpesito más tonto");
-
-       if(colision.gameObject.tag=="Gol1"){
-
-           Debug.Log("Gol para jugador 1");
-       
-       }else if(colision.gameObject.tag=="Gol2"){
-       
-           Debug.Log("Gol para jugador 2");
-       
-       }else if(colision.gameObject.tag=="Player"){
            if(speed>0){
+
                speed=-speed;
+
                Debug.Log("Velocidad: "+speed);
+
            }else if(speed<0){
 
                speed=-speed;
+
                Debug.Log("Velocidad: "+speed);
+
            }
        }
     }
