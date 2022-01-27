160dc72c52f6801812870a1dc7586a6447b4042a
Santiago López Rey <devsantilopez@gmail.com>
2022-01-27 20:46:39 +0100

    Añadida física para rebote de bola

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -39,7 +39,7 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+        this.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, speed);
 
         CheckBall();
 
@@ -47,11 +47,13 @@ public class Ball : MonoBehaviour
     }
 
     public void OnCollisionEnter2D(Collision2D colision) {
+
+        speed=-speed;
  
         if(colision.gameObject.tag=="Player"){
 
-            speed=-speed;
-            
+            //speed=-speed;
+
         }
     }
 
